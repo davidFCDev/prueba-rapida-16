9680fc64be2513117ef775ef6ad6e97456727adc
David Fernández <davidfernandezcomesana@gmail.com>
2023-08-03 17:46:30 +0200

    base

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <div className="page">
+      <header>
+        <h1>React App</h1>
+        <form>
+          <input type="text" placeholder="Matrix, avengers..." />
+          <button>Search</button>
+        </form>
+      </header>
+      <main>Lista de peliculas</main>
+    </div>
+  );
 }
 
 export default App;
