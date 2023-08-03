d27e6f32b91398e3af0022a8ff6782053ff287fc
David Fernández <davidfernandezcomesana@gmail.com>
2023-08-03 17:48:08 +0200

    render movies

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import "./App.css";
+import responseMovies from "./mocks/with-results.json";
 
 function App() {
+  const movies = responseMovies.results;
+  const hasMovies = movies?.length > 0;
+
   return (
     <div className="page">
       <header>
@@ -10,7 +14,21 @@ function App() {
           <button>Search</button>
         </form>
       </header>
-      <main>Lista de peliculas</main>
+
+      <main>
+        {hasMovies ? (
+          <ul>
+            {movies.map((movie) => (
+              <li key={movie.id}>
+                <h2>{movie.title}</h2>
+                <p>{movie.overview}</p>
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <p>No movies found</p>
+        )}
+      </main>
     </div>
   );
 }
