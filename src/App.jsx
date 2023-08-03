3fb80740b5eee0537e0ebd5a511eed5ccea60882
David Fernández <davidfernandezcomesana@gmail.com>
2023-08-03 17:57:17 +0200

    control de eventos

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import { useState } from "react";
 import "./App.css";
 import { Movies } from "./components/Movies";
 import responseMovies from "./mocks/with-results.json";
 
 function App() {
+  const [search, updateSearch] = useState("");
   const movies = responseMovies.Search;
 
   const mappedMovies = movies.map((movie) => {
@@ -14,12 +16,28 @@ function App() {
     };
   });
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("submit", search);
+  };
+
+  const handleChange = (event) => {
+    const newSearch = event.target.value;
+    updateSearch(newSearch);
+  };
+
   return (
     <div className="page">
       <header>
         <h1>React App</h1>
-        <form>
-          <input type="text" placeholder="Matrix, avengers..." />
+        <form onSubmit={handleSubmit}>
+          <input
+            onChange={handleChange}
+            value={search}
+            name="query"
+            type="text"
+            placeholder="Matrix, avengers..."
+          />
           <button>Search</button>
         </form>
       </header>
